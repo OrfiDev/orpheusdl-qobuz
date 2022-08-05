6f0109f626ddd43f853d4b0ad006dfe1ccf017fe
mexsana123 <mexsana123@gmail.com>
2022-08-05 09:51:53 -0500

    Added description to `AlbumInfo`

--- interface.py
+++ interface.py
@@ -1,4 +1,5 @@
-import unicodedata
+import unicodedata, re
+from html import unescape
 from datetime import datetime
 
 from utils.models import *
@@ -34,6 +35,7 @@ class ModuleInterface:
         }
         self.quality_tier = module_controller.orpheus_options.quality_tier
         self.quality_format = settings.get('quality_format')
+        self.parse_html = lambda description: re.sub(r'<.*?>', '', unescape(description))
 
     def login(self, email, password):
         token = self.session.login(email, password)
@@ -157,6 +159,7 @@ class ModuleInterface:
             release_year = int(album_data['release_date_original'].split('-')[0]),
             explicit = album_data['parental_warning'],
             quality = self.quality_format.format(**quality_tags) if self.quality_format != '' else None,
+            description = self.parse_html(album_data['description']) if album_data.get('description') else None,
             cover_url = album_data['image']['large'].split('_')[0] + '_org.jpg',
             booklet_url = booklet_url,
             track_extra_kwargs = {'data': extra_kwargs}
